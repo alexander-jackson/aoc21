2b5c0f77937a7919a674e16b1eaefc83c57247ce
Alexander Jackson <alex.jackson@starlingbank.com>
2021-12-08 21:12:34 +0000

    feat: begin generating lines for Day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,9 +1,27 @@
+use std::collections::HashSet;
+
 use nom::{
     bytes::complete::tag, character::complete::newline, multi::separated_list1,
     sequence::separated_pair, IResult,
 };
 
 #[derive(Copy, Clone, Debug)]
+enum Direction {
+    Horizontal,
+    Vertical,
+}
+
+impl Direction {
+    fn from(start: Point, end: Point) -> Direction {
+        if start.x != end.x {
+            Direction::Horizontal
+        } else {
+            Direction::Vertical
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 struct Point {
     x: u32,
     y: u32,
@@ -33,6 +51,50 @@ impl Line {
 
         Ok((input, Self { start, end }))
     }
+
+    fn points(&self) -> Vec<Point> {
+        let direction = Direction::from(self.start, self.end);
+
+        // Swap if needed
+        let (start, end) = match direction {
+            Direction::Horizontal => {
+                if self.start.x > self.end.x {
+                    (self.end, self.start)
+                } else {
+                    (self.start, self.end)
+                }
+            }
+            Direction::Vertical => {
+                if self.start.y > self.end.y {
+                    (self.end, self.start)
+                } else {
+                    (self.start, self.end)
+                }
+            }
+        };
+
+        // Iterate based on the direction
+        match direction {
+            Direction::Horizontal => {
+                let diff = end.x - start.x;
+                (0..=diff)
+                    .map(|i| Point {
+                        x: start.x + i,
+                        y: start.y,
+                    })
+                    .collect()
+            }
+            Direction::Vertical => {
+                let diff = end.y - start.y;
+                (0..=diff)
+                    .map(|i| Point {
+                        x: start.x,
+                        y: start.y + i,
+                    })
+                    .collect()
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -46,11 +108,91 @@ impl Input {
 
         Ok((input, Self { lines }))
     }
+
+    fn calculate_dangerous_point_count(&self) -> usize {
+        let mut covered_points = HashSet::new();
+        let mut dangerous_points = HashSet::new();
+
+        self.lines.iter().for_each(|line| {
+            for point in line.points() {
+                println!("Line {:?} produced point {:?}", line, point);
+
+                if !covered_points.insert(point) {
+                    dangerous_points.insert(point);
+                }
+            }
+        });
+
+        dbg!(&dangerous_points);
+
+        dangerous_points.len()
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (_, input) = Input::parse(include_str!("../sample.txt"))?;
-    dbg!(&input);
+    let dangerous_count = input.calculate_dangerous_point_count();
+    dbg!(&dangerous_count);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lines_are_as_expected() {
+        let line = Line {
+            start: Point { x: 5, y: 5 },
+            end: Point { x: 5, y: 8 },
+        };
+        let expected = vec![
+            Point { x: 5, y: 5 },
+            Point { x: 5, y: 6 },
+            Point { x: 5, y: 7 },
+            Point { x: 5, y: 8 },
+        ];
+
+        assert_eq!(line.points(), expected);
+
+        let line = Line {
+            start: Point { x: 5, y: 8 },
+            end: Point { x: 5, y: 5 },
+        };
+        let expected = vec![
+            Point { x: 5, y: 5 },
+            Point { x: 5, y: 6 },
+            Point { x: 5, y: 7 },
+            Point { x: 5, y: 8 },
+        ];
+
+        assert_eq!(line.points(), expected);
+
+        let line = Line {
+            start: Point { x: 5, y: 5 },
+            end: Point { x: 8, y: 5 },
+        };
+        let expected = vec![
+            Point { x: 5, y: 5 },
+            Point { x: 6, y: 5 },
+            Point { x: 7, y: 5 },
+            Point { x: 8, y: 5 },
+        ];
+
+        assert_eq!(line.points(), expected);
+
+        let line = Line {
+            start: Point { x: 8, y: 5 },
+            end: Point { x: 5, y: 5 },
+        };
+        let expected = vec![
+            Point { x: 5, y: 5 },
+            Point { x: 6, y: 5 },
+            Point { x: 7, y: 5 },
+            Point { x: 8, y: 5 },
+        ];
+
+        assert_eq!(line.points(), expected);
+    }
 }
