e03fd120564b8283b0af1b0c9f990e488515e2c6
Alexander Jackson <alexanderjackson@protonmail.com>
2021-12-09 07:04:36 +0000

    feat: add solution to Part 1 for Day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -22,12 +22,67 @@ impl DepthMap {
     fn parse(input: &str) -> IResult<&str, Self> {
         nom::combinator::map(separated_list1(newline, digit1), Self::new)(input)
     }
+
+    fn no_point_lower(&self, x: usize, y: usize) -> bool {
+        // Check surroundings if they exist
+        let points = vec![
+            (x.checked_sub(1), Some(y)),
+            (Some(x + 1), Some(y)),
+            (Some(x), y.checked_sub(1)),
+            (Some(x), Some(y + 1)),
+        ];
+
+        let centre = self.values[x][y];
+
+        for p in points {
+            let (x, y) = match p {
+                (Some(_), None) | (None, Some(_)) => continue,
+                (Some(x), Some(y)) => (x, y),
+                _ => unreachable!(),
+            };
+
+            if self.values.len() <= x {
+                continue;
+            }
+
+            if self.values[x].len() <= y {
+                continue;
+            }
+
+            // Access should be fine here
+            if self.values[x][y] <= centre {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn lowest_points(&self) -> Vec<u32> {
+        let mut lowest = Vec::new();
+
+        for x in 0..self.values.len() {
+            for y in 0..self.values[x].len() {
+                let v = self.values[x][y];
+                let is_lowest = self.no_point_lower(x, y);
+
+                if is_lowest {
+                    lowest.push(v);
+                }
+            }
+        }
+
+        lowest
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input = include_str!("../sample.txt");
+    let input = include_str!("../input.txt");
     let depth_map = DepthMap::parse(input)?.1;
-    dbg!(&depth_map);
+
+    let lowest_points = depth_map.lowest_points();
+    let answer: u32 = lowest_points.iter().map(|p| p + 1).sum();
+    dbg!(&answer);
 
     Ok(())
 }
